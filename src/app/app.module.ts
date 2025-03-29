96d803427122efa7814a7305b431e88b52c35f1e
Evgeny_MKH <trisemerka@gmail.com>
2025-03-29 20:46:56 +0500

    Создание компоненСозданиеÐ компÐ а каждю ÑÑСоздание компонÐнентов для каждой страницы

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/pages/main/main.component';
+import { OrderComponent } from './components/pages/order/order.component';
+import { ProductsComponent } from './components/pages/products/products.component';
+import { ProductComponent } from './components/pages/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    OrderComponent,
+    ProductsComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
