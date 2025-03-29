24ccec5050581bc5354fd6383564418d42bc3016
Evgeny_MKH <trisemerka@gmail.com>
2025-03-29 23:42:05 +0500

    Добавление диалогового окна на страницу main, логика и стили

--- src/app/components/pages/main/main.component.ts
+++ src/app/components/pages/main/main.component.ts
@@ -1,16 +1,51 @@
+import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+
 // Подключение JQuery
 declare var $: any;
 // Импортируем Accordion
 import 'jquery-ui/ui/widgets/accordion';
-import {AfterViewInit, Component} from '@angular/core';
+import {delay, of, Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'main-component',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements AfterViewInit {
-  ngAfterViewInit() {
+
+export class MainComponent implements AfterViewInit, OnInit, OnDestroy {
+  // Получаем доступ к диалоговому окну
+  @ViewChild('myDialog') dialog: any;
+  // Указываем что диалоговое окно не показывается при загрузке страницы
+  showDialog: boolean = false;
+  // Создали переменную для управления отмены подписок на Observable
+  private destroy$ = new Subject<void>();
+
+  ngOnInit(): void {
+    of(null).pipe(
+        // Установил задержку в 10сек
+        delay(10000),
+        takeUntil(this.destroy$)
+    ).subscribe((): void => {
+      // Проверяем существование диалога, если есть то вызываем функцию
+      if (this.dialog) {
+       this.dialog.nativeElement.showModal();
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    // Убираем подписку
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  closeDialog(): void {
+    if (this.dialog) {
+      this.dialog.nativeElement.close();
+    }
+  }
+
+  ngAfterViewInit(): void {
     // Настройка и инициализация `accordion`
     ($('#accordion') as any).accordion({
       collapsible: true,
@@ -21,7 +56,7 @@ export class MainComponent implements AfterViewInit {
     // Добавление в `accordion` стрелочки
     $(document).ready((): void => {
       $('.accordion-header').click(function (this: HTMLElement): void {
-        let $header = $(this); // Теперь this — это кликнутый .accordion-header
+        let $header = $(this);
         let $body = $header.next('.accordion-body');
 
         if ($header.hasClass('collapsed')) {
