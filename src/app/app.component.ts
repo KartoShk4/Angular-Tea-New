4ff2225b9dc6f73cbf4de452587c6b06ef3d3e39
Evgeny_MKH <trisemerka@gmail.com>
2025-03-29 19:23:36 +0500

    Востановление проекта после переноса на Angular, запуск стидилей и скриптов

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+// Подключение JQuery
+declare var $: any;
+
+import {AfterViewInit, Component} from '@angular/core';
+// Импортируем Accordion
+import 'jquery-ui/ui/widgets/accordion';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tea';
+export class AppComponent implements AfterViewInit{
+  ngAfterViewInit() {
+    // Настройка и инициализация `accordion`
+    ($('#accordion') as any).accordion({
+      collapsible: true,
+      active: false,
+      icons: false,
+    });
+
+    // Добавление в `accordion` стрелочки
+    $(document).ready((): void => {
+      $('.accordion-header').click(function (this: HTMLElement): void {
+        let $header = $(this); // Теперь this — это кликнутый .accordion-header
+        let $body = $header.next('.accordion-body');
+
+        if ($header.hasClass('collapsed')) {
+          // Закрываем все открытые блоки
+          $('.accordion-header').addClass('collapsed');
+          $('.accordion-body').removeClass('show');
+
+          // Открываем текущий блок
+          $header.removeClass('collapsed');
+          $body.addClass('show');
+        } else {
+          // Закрываем текущий блок
+          $header.addClass('collapsed');
+          $body.removeClass('show');
+        }
+      });
+    });
+  }
 }
+
+
